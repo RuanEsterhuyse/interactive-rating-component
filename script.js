f7ab5917f5628be82fba4b55ca39a4223878743f
Ruan Esterhuyse <ruan.esterhuyse@gmail.com>
2022-05-19 11:35:49 +0200

    finish style

---
+++ script.js
@@ -0,0 +1,16 @@
+let btns = document.querySelectorAll('.btn-container button');
+let card = document.querySelector('.main-card');
+let secondCard = document.querySelector('.second-main-card');
+let span = document.querySelector('span');
+
+
+// Array.from(btns).forEach(btn => {
+//     btn.addEventListener('click', e =>{
+//         const rating = e.target.textContent
+//         span.textContent = rating
+//         card2.classList.remove('hide')
+//         card.classList.add('hide')
+//     });
+   
+// });
+
