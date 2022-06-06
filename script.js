422bf4ca8a966dd43f9aa10b6eb4fa664d2af08a
Ruan Esterhuyse <ruan.esterhuyse@gmail.com>
2022-06-06 20:34:07 +0200

    update

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let btns = document.querySelectorAll(".btn");
 let starsScore = 3;
 
 
-
 submitButton.addEventListener('click', btnClicked);
 btns.forEach(btn =>{
     btn.addEventListener('click', ratingBtnClicked);
@@ -17,43 +16,19 @@ function btnClicked(){
     mainCard.classList.add("hide");
     secondMainCard.classList.remove("hide");
     numberUpdate.textContent = starsScore;
-
 };
-
-
 function ratingBtnClicked (e) {
     btns.forEach( btn => {
         btn.classList.remove('active');
     })
-    
     e.target.classList.add('active');
-
     starsScore = e.target.textContent;
 };  
-
-
-// for (button of btns) {
-//     button.addEventListener("click", function (e) {
-//       e.preventDefault;
-//       let value = button.value;
-//       numberUpdate.innerText = `${value}`;
-//       submitButton.disabled = false;
-//     });
-//   }
-//   console.log(value);
-
-
-// submitButton.addEventListener("click", clickSubmit);
-// function clickSubmit() {
-//     mainCard.classList.add("hide");
-//     secondMainCard.classList.remove("hide");
-
-//     }
 
 secondImage.addEventListener("click",backToRatings);
 function backToRatings() {
     secondMainCard.classList.add("hide");
     mainCard.classList.remove("hide");
-}
+};
 
 
